df8a8e609e0509be4855f3851362b19da9cc8147
john-c-landry <johnclandry@yahoo.com>
2026-04-24 13:45:07 -0400

    x

--- client/src/components/ProjectCard.jsx
+++ client/src/components/ProjectCard.jsx
@@ -1,11 +1,11 @@
 const ProjectCard = ({ project }) => (
-  <div className="bg-gray-300 shadow-md p-3 rounded-lg font-body">
-    <div className="mb-0 font-display text-3xl italic tracking-tighter">{project.title}</div>
-    <div className="mb-4 font-body text-lg leading-tight">{project.description}</div>
-    <span className="font-label text-xs tracking-wide">TECHNOLOGIES:</span>
+  <div className="p-3 bg-gray-300 rounded-lg shadow-md font-body">
+    <div className="mb-0 text-3xl italic tracking-tighter font-display">{project.title}</div>
+    <div className="mb-4 text-lg leading-tight font-body">{project.description}</div>
+    <span className="py-2 text-xs tracking-wide font-label">TECHNOLOGIES:</span>
     <div className="flex flex-wrap gap-2 mt-0 mb-5">
       {project.tech.map((tech, index) => (
-        <span key={index} className="px-2 py-1 rounded font-body text-lg">
+        <span key={index} className="px-2 py-0 text-lg rounded font-body">
           {tech}
         </span>
       ))}
@@ -17,7 +17,7 @@ const ProjectCard = ({ project }) => (
         rel="noopener noreferrer"
         className="btn btn-primary"
       >
-        Live Demo
+        LIVE DEMO
       </a>
       <a
         href={project.githubUrl}
@@ -25,7 +25,7 @@ const ProjectCard = ({ project }) => (
         rel="noopener noreferrer"
         className="btn btn-primary"
       >
-        GitHub
+        GITHUB
       </a>
     </div>
   </div>
