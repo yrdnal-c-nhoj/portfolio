2353d3ffaf2fb05426e0825fe47e684ce0ecd438
john-c-landry <johnclandry@yahoo.com>
2026-04-24 13:14:38 -0400

    x

--- server/server.js
+++ server/server.js
@@ -18,8 +18,6 @@ const app = express();
 
 // CORS configuration
 const allowedOrigins = [
-  'https://portfolio-sand-mu-xkv3dqgohg.vercel.app',
-  'https://portfolio-k7acescoa-johns-projects-75897040.vercel.app',
   'http://localhost:5173',
   'http://localhost:5174',
   'http://localhost:5175',
